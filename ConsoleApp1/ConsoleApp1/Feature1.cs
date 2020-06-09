10784701f92b5f1d21a6f16646e9919bd98c4241
Amita Rashyani <amita.rashyani@gmail.com>
2020-06-09 23:11:42 +0100

    Added the Add method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Feature1
+    public class Feature1
     {
+
+        public int Add() 
+        {
+
+            int x = 1;
+            int y = 2;
+            int sum = x + y;
+            return sum;
+        
+        }
+        
     }
 }
