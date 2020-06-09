05b4357ee3bf53465ff9c904647a3cd78e73bfca
AmitaRashyani <36245415+AmitaRashyani@users.noreply.github.com>
2020-06-09 22:34:42 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Code was added in the Github 
+            // Code was edited in the Github 
         }
     }
 }
